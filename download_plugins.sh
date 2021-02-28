5cec05060baccd7b413e6ef8d8649cd3f6db6f0e
jiseongg <jiseongg301@gmail.com>
2021-02-28 22:03:04 +0900

    divide init.sh to two files

--- download_plugins.sh
+++ download_plugins.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env sh
-apt-get update
-apt-get install -y vim ctags cscope
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim 
 
---
+++ install_packages.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+apt-get update
+apt-get install -y vim ctags cscope
+
